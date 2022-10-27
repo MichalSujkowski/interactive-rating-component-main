ff748a6777eb0a6fbd6bb51f477eebd01549ec49
Michał Sujkowski <michalsujkowski.fd@gmail.com>
2022-10-27 13:47:02 +0200

    add js function to event handling when you want to rate one more time

--- app.js
+++ app.js
@@ -3,16 +3,25 @@ const thanksContainer = document.querySelector('.thanks');
 const submitBTN = document.querySelector('.button');
 const rates = document.querySelectorAll('.rate');
 const rateSpan = document.querySelector('span');
+const thankBtn = document.querySelector('i');
 
 
 submitBTN.addEventListener('click', () => {
     thanksContainer.classList.add('actived');
+    rateContainer.classList.remove('actived');
 });
+
+thankBtn.addEventListener('click', () => {
+    rateContainer.classList.add('actived');
+    thanksContainer.classList.remove('actived');
+})
 
 rates.forEach((rate) => {
     rate.addEventListener('click', () => {
         rateSpan.innerText = rate.innerText
     });
 });
+
+
 
 
